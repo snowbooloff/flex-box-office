26519ca4db2935222e037808f0de689a3c724602
Dmitriy Konoplev <snowbooloff163@gmail.com>
2022-10-30 01:22:55 +0300

    Fixed randomomizeColorObject function

---
+++ js/randmizeLevel.js
@@ -0,0 +1,61 @@
+const CommandArray = [
+    (justifyContentList = [
+        "justify-content: flex-start;",
+        "justify-content: flex-end;",
+        "justify-content: center;",
+        "justify-content: space-between;",
+        "justify-content: space-around;",
+        "justify-content: space-evenly;",
+    ]),
+    (alignItemsList = [
+        "align-items: flex-start;",
+        "align-items: flex-end;",
+        "align-items: center;",
+        "align-items: stretch;",
+        "align-items: baseline;",
+    ]),
+    (flexDirectionList = [
+        "flex-direction: row-reverse;",
+        "flex-direction: column;",
+        "flex-direction: column-reverse;",
+    ]),
+    (flexWrapList = ["flex-wrap: wrap;", "flex-wrap: wrap-reverse;"]),
+    (alignContentList = [
+        "align-content: flex-start;",
+        "align-content: flex-end;",
+        "align-content: center;",
+        "align-content: space-between;",
+        "align-content: space-around;",
+        "align-content: space-evenly;",
+        "align-content: stretch;",
+    ]),
+];
+
+function generateRandomLevel(array) {
+    let CommandsString = "";
+    for (let i = 0; i < array.length; i++) {
+        let arrayChild = array[i];
+        let randomCommand = Math.floor(Math.random() * array[i].length);
+        CommandsString += arrayChild[randomCommand];
+    }
+    return CommandsString;
+}
+
+function randomomizeObject() {
+    let randomInteger = 3 + Math.random() * (6 - 3);
+    return Math.floor(randomInteger);
+}
+
+function randomomizeColorObject() {
+    let engineer = document.querySelector('.engineer');
+    var objectColors = ["blue", "green", "yellow"];
+    for (let i = 0; i<= objectColors.length; i++) {
+        if (engineer !== null) {
+            if (engineer.classList.contains(objectColors[i])) {
+                objectColors.splice(i, 1)
+            }
+        }
+    }
+    let randomInteger = Math.floor(Math.random() * objectColors.length);
+    return objectColors[randomInteger];
+}
